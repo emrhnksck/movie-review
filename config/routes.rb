bb77b553b2b6f155b22dae56f34dcea254ce45cc
emrehan <emrehan.kisacik@hotmail.com>
2022-02-09 16:56:29 +0000

    added moevie scaffold and devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "movies#index"
   # Defines the root path route ("/")
   # root "articles#index"
+    resources :movies
 end
