3d9e227de5022f1310a479af2b2fb49fdaefa8a1
emrehan <emrehan.kisacik@hotmail.com>
2022-02-11 00:58:29 +0300

    added comment ability to movies

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.create(content: comment_params[:content], user_id: params[:user_id] , movie_id: params[:movie_id])
+  end
+
+
+private
+def comment_params
+  params.require(:comment).permit(:content)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
+
 
   Rails.application.routes.draw do
   resources :comments
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   root "movies#index"
   # Defines the root path route ("/")
   # root "articles#index"
-    resources :movies
+    resources :movies do
+      resources :comments
+    end
 end
